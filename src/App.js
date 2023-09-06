d5dcb9d14dbae6ff4d7a59475618e3688a96ef5f
unknown <vitory150518@gmail.com>
2023-09-06 15:53:49 -0300

    Formulario_User

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logohs from './assets/logohs.png';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">Bem vindo</span>
+
+            <span className="login-form-title">
+              <img src={logohs} alt='logohs'/>
+            </span>
+
+            <div className='wrap-input'>
+              <input type='email' />
+              <span className='focus-input'>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
